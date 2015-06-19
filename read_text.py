c81f32e686d6921eec13b7750257e964104e30a1
Ryan Teoh <teohtzejun@gmail.com>
2015-06-19 12:20:00 +0800

    second

--- read_text.py
+++ read_text.py
@@ -2,11 +2,9 @@ from sys import argv
 import re
 
 script, filename, keyword = argv
-pattern = ['this','that']
-regex = re.compile("f\(\s*([^,]+)\s*,\s*([^,]+)\s*\)")
 with open(filename) as f:
     for line in f:
-        result = regex.search(pattern,line)
+        result = re.search('is',line)
         print "search found %r:" % result
 
 print "Here's your file %r:" % filename
