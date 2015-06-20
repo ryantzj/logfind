b966a6ea1596db153b32653e4d9f9a4d06c87314
Ryan Teoh <teohtzejun@gmail.com>
2015-06-20 14:17:28 +0800

    fifth

--- read_text3.py
+++ read_text3.py
@@ -1,17 +1,14 @@
 from sys import argv
 import re
 
-script, filename, keyword = argv
-names = keyword
-print(sys.argv)
+script, filename, keyword, keyword2 = argv
+names = keyword+"|"+keyword2
 p = re.compile(names,re.I)
-print "%r" % names
 
 
 with open(filename) as f:
     for line in f:
-		print "%r" % line
-	        m =	p.findall(line)
-		print(m)
+	print(line)
+        m =	p.findall(line)
+	print(m)
 print "Here's your file %r:" % filename
-print "keyword is:", keyword
